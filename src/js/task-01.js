934b09a44164560f4c8332b7683ac200d4c5b98a
khvostikov vadim <khvostikov212121@gmail.com>
2023-02-19 12:10:39 +0200

    Виконує перше завдання

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -7,15 +7,3 @@ itemEl.forEach((element) => {
   console.log("Category:", titleEl.textContent);
   console.log("Elements:", lengthLiEl);
 });
-// const categoriesRef = document.querySelectorAll('li.item');
-
-// const numberOfCategoriesRef = categoriesRef.length;
-
-// console.log(`Number of categories: ${numberOfCategoriesRef}`);
-
-// const categoryRef = categoriesRef.forEach(el => {
-//     const category = el.firstElementChild.textContent;
-//     const elements = el.lastElementChild.children.length;
-//     console.log(`Category: ${category} 
-// Elements: ${elements}`)
-// });
