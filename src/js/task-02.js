29b0523c9a1d90c5280d40a5ea83b5bc2b7c8fed
khvostikov vadim <khvostikov212121@gmail.com>
2023-02-20 21:43:00 +0200

    Виконує друге завдання

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const liElArray = [];
+ingredients.forEach((element) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = element;
+  // console.log(liEl);
+  liElArray.push(liEl);
+});
+
+list.prepend(...liElArray);
+
+// const list = document.querySelector(".ingredients");
+// let newArray = []
+// ingredients.forEach((element) => {
+//   const liEl = document.createElement("li");
+//   liEl.classList.add("item");
+//   liEl.textContent = element;
+//   newArray.push(liEl);
+// });
+// list.appendChild(newArray);
