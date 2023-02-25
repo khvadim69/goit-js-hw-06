3dc76d313ad05dd567b5eef797358c99159ed575
khvostikov vadim <khvostikov212121@gmail.com>
2023-02-25 16:52:06 +0200

    Виконує 8 завдання

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,14 +1,8 @@
-
-
 const input = document.querySelector("#font-size-control");
 const span = document.querySelector("#text");
 // span.style.fontSize = `${input.value}px`;
 
-input.addEventListener('input', (event) => {
-     console.log(event);
-span.style.fontSize = `${event.target.value}px`;
-
+input.addEventListener("input", (event) => {
+  console.log(event);
+  span.style.fontSize = `${event.target.value}px`;
 });
-
-
-
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
+  if (email.value === "" || password.value === "") {
+    alert("Будь ласка заповніть форму!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+});
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+
+//   if (email.value === "" || password.value === "") {
+//     alert("Будь ласка заповніть форму!");
+//   }
+
+//   console.log(`Login: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
