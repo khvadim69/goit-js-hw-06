6e36d5d02d6c7788a20408bbedca7bf0c33302be
khvostikov vadim <khvostikov212121@gmail.com>
2023-02-26 17:57:49 +0200

    трохи підкорегував

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -7,14 +7,14 @@
 // };
 // button.addEventListener("click", handleClick);
 const ref = {
-  decrement : document.querySelector('[data-action="decrement"]'),
-  increment : document.querySelector('[data-action="increment"]'),
-  span : document.querySelector('#value'),
+  decrement: document.querySelector('[data-action="decrement"]'),
+  increment: document.querySelector('[data-action="increment"]'),
+  span: document.querySelector("#value"),
 };
-ref.decrement.addEventListener('click' , () =>{
+ref.decrement.addEventListener("click", () => {
   ref.span.textContent -= 1;
 });
-ref.increment.addEventListener('click' , () => {
+ref.increment.addEventListener("click", () => {
   let counterValue = parseInt(ref.span.textContent);
   ref.span.textContent = counterValue + 1;
 });
--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,15 +1,23 @@
-const inputName =
-  document.querySelector("#name-input");
-const nameOutput =
-  document.querySelector(
-    "#name-output"
-  );
+// const inputName =
+//   document.querySelector("#name-input");
+// const nameOutput =
+//   document.querySelector(
+//     "#name-output"
+//   );
 
-inputName.addEventListener(
-  "input",
-  () =>
-    nameOutput.innerText =
-      inputName.value
-        ? inputName.value
-        : "Anonymous")
+// inputName.addEventListener(
+//   "input",
+//   () =>
+//     nameOutput.innerText =
+//       inputName.value
+//         ? inputName.value
+//         : "Anonymous")
 
+const inputName = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+inputName.addEventListener("input", (event) => {
+  nameOutput.textContent = event.currentTarget.value.trim();
+  if (nameOutput.textContent === "") {
+    nameOutput.textContent = "Anonymous";
+  }
+});
